f0413ea7ea0d29789866cc9a03b479918b0c9956
内田 匠 <t_uchida@r.recruit.co.jp>
2020-01-27 13:12:33 +0900

    in submodule

--- src/my_simple_mecab.py
+++ src/my_simple_mecab.py
@@ -7,6 +7,33 @@ import pandas as pd, numpy as np
 
 class keitaiso:
     def __init__(self, use_PoW=['名詞','動詞','形容詞','副詞',], stop_words=[], use_words=[], user_dic_files=[]):
+        """
+        ARGUMENT
+        ----------------
+        use_PoW [list]:
+            .tokenize(text) で出力される品詞を指定する。
+            ex) use_PoW=['名詞','動詞'] なら名詞と動詞しか出力しない。
+        stop_words [list]:
+            .tokenize(text) で出力しない単語を指定する。
+        use_words [list]:
+            .tokenize(text) で出力する単語の集合を指定する。
+            defaultは空リスト([])であり、この場合は全ての単語が出力される。
+        user_dic_files [list]:
+            Mecabが形態素解析に用いるユーザー辞書を指定する場合に用いる。            
+        
+        EXAMPLE
+        ----------------
+        text = "今日は何時に帰ってきますか？　と彼女は尋ねてくる"
+        use_PoW = ['名詞','動詞','形容詞','副詞','記号',]
+        stop_words = ['*','よう','上','の','様','こちら','際','ところ','はず','\u3000',]
+    
+        k = keitaiso(use_PoW=use_PoW,stop_words=stop_words)
+        print(k.basic_tokenize(text))
+         > [[0, '名詞', '今日'], [1, '助詞', 'は'], [2, '名詞', '何'], [3, '名詞', '時'], [4, '助詞', 'に'], [5, '動詞', '帰る'], [6, '助詞', 'て'], [7, '動詞', 'くる'], [8, '助動詞', 'ます'], [9, '助詞', 'か'], [10, '記号', '？'], [11, '記号', '\u3000'], [12, '助詞', 'と'], [13, '名詞', '彼女'], [14, '助詞', 'は'], [15, '動詞', '尋ねる'], [16, '助詞', 'て'], [17, '動詞', 'くる']]
+        print(k.tokenize(text))
+         > [0, '名詞', '今日'], [2, '名詞', '何'], [3, '名詞', '時'], [5, '動詞', '帰る'], [7, '動詞', 'くる'], [10, '記号', '？'], [13, '名詞', '彼女'], [15, '動詞', '尋ねる'], [17, '動詞', 'くる']]
+        
+        """
         self.use_PoW = use_PoW
         self.stop_words = stop_words
         self.use_words = use_words
